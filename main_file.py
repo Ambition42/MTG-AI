56ebce24695107df1cc40d123fbcbdff8cfa6096
Ambition42 <gabriel.doyen.12@gmail.com>
2025-01-15 17:08:11 +0100

    Just a simple threading issue...

--- main_file.py
+++ main_file.py
@@ -1,10 +1,23 @@
-import requests
-
-print("helloworld")
-print("Voici une seconde ligne de code tout aussi inutile :)")
+import threading
 
 
-class ARandomClasse:
-    def __init__(self):
-        pass
-        
+def increment_score():
+    global score
+    for x in range(1000000):
+        score += 1
+
+
+def decrement_score():
+    global score
+    for x in range(1000000):
+        score -= 1
+
+
+score = 0
+first_thread = threading.Thread(target=increment_score)
+second_thread = threading.Thread(target=decrement_score)
+
+first_thread.start()
+second_thread.start()
+
+print(f"Le score est égal à {score}")
